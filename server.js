9feaedb99827e14e567db99c24e3e656376ad880
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-09-07 14:18:19 -0400

    Added README

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  process.env.MONGODB_URI ||
+    "mongodb+srv://sullivan:abundance26$@cluster0.abkvb.mongodb.net/google-books-search?retryWrites=true&w=majority",
   {
     useCreateIndex: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
   }
 );
 
